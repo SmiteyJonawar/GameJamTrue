849dfdbb5a023ea2cbc1d90f28520d81dad5c8af
SmiteyJonawar <jonasr16@student.sdu.dk>
2018-10-06 18:46:56 +0200

    Initial commit

---
+++ core/src/gamejam/spacehunter/CombatScreen.java
@@ -0,0 +1,50 @@
+package gamejam.spacehunter;
+
+
+import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+
+public class CombatScreen implements Screen {
+
+    SpriteBatch batch;
+
+    public CombatScreen(){
+        batch = new SpriteBatch();
+    }
+
+    @Override
+    public void show() {
+
+    }
+
+    @Override
+    public void render(float delta) {
+
+    }
+
+    @Override
+    public void resize(int width, int height) {
+
+    }
+
+    @Override
+    public void pause() {
+
+    }
+
+    @Override
+    public void resume() {
+
+    }
+
+    @Override
+    public void hide() {
+
+    }
+
+    @Override
+    public void dispose() {
+
+    }
+}
---
+++ core/src/gamejam/spacehunter/Player.java
@@ -0,0 +1,4 @@
+package gamejam.spacehunter;
+
+public class Player {
+}
